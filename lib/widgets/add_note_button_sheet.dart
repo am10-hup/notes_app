82d430c83df2e0845c671b9f71bcb0179be9ef26
alimostafa <am1078253@gmail.com>
2024-12-29 13:53:41 +0200

    ctreate add note cubit

--- lib/widgets/add_note_button_sheet.dart
+++ lib/widgets/add_note_button_sheet.dart
@@ -66,7 +66,6 @@ class _AddNoteFormState extends State<AddNoteForm> {
                 autovalidateMode = AutovalidateMode.always;
                 setState(() {});
               }
-              ;
             },
           ),
           SizedBox(
